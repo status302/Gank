2927a22aa1bf57f170e1e56464eb0b99c4cc40d0
Qiuncheng <qiuncheng@gmail.com>
2016-05-20 17:45:45 +0800

    使用了Alamofire

---
+++ Gank/Gank/EveryDayGank/QCEveryDayGnakViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QCEveryDayGnakViewController.swift
+//  Gank
+//
+//  Created by 程庆春 on 16/5/20.
+//  Copyright © 2016年 qiuncheng.com. All rights reserved.
+//
+
+import UIKit
+
+class QCEveryDayGnakViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
