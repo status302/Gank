aaadfd87a0cbb0b437666a1fb4c20990074ab69d
Qiuncheng <qiuncheng@gmail.com>
2016-08-14 09:57:50 +0800

    new DayResult.swift

---
+++ Gank/DayResult.swift
@@ -0,0 +1,14 @@
+//
+//  DayResult.swift
+//  Gank
+//
+//  Created by 程庆春 on 16/8/14.
+//  Copyright © 2016年 qiuncheng.com. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class DayResult: Object {
+
+}
