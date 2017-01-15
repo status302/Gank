4beedd0f0bd336e6ef588fe7ce60065cd9826749
Qiuncheng <qiuncheng@gmail.com>
2017-01-15 22:39:53 +0800

    Update folder.

---
+++ Gank/RootViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Gank
+//
+//  Created by 程庆春 on 2017/1/8.
+//  Copyright © 2017年 Qiun Cheng. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UIApplication.shared.isStatusBarHidden = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
